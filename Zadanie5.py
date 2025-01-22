870dbf0f4083f54936760ade2febdea9fc6850ff
Rafal Przetakowski <rafal.p@beeflow.co.uk>
2025-01-22 17:52:53 +0000

    Reformat kodu + dodanie main() dla zgodności dla zgodności z PEP i dobrymi praktykami + dodana informacja o typie zwracanym w `animal_sound`

--- Zadanie5.py
+++ Zadanie5.py
@@ -6,35 +6,46 @@
 
 from abc import ABC, abstractmethod
 
+
 class Animal(ABC):
     @abstractmethod
-    def make_sound(self):
+    def make_sound(self) -> str:
         pass
+
 
 class Dog(Animal):
     def make_sound(self):
         return "Woof!"
 
+
 class Cat(Animal):
-    def make_sound(self):
+    def make_sound(self) -> str:
         return "Meow!"
 
+
 class Cow(Animal):
-    def make_sound(self):
+    def make_sound(self) -> str:
         return "Mooo!"
 
-dog = Dog()
-cat = Cat()
-cow = Cow()
 
 def animal_sounds(animals: list) -> None:
     for animal in animals:
         print(animal.make_sound())
 
-print(animal_sounds([dog, cat, cow]))
+
+# Wywołanie kody powinno znaleźć się w funkcji main ;)
+def main() -> None:
+    dog = Dog()
+    cat = Cat()
+    cow = Cow()
+
+    # Już funkcja `animal_sound` printuje informacje, więc poniżej print jedyne co robi, to
+    # wyświetla na ekranie `None` jako ostatnią informację
+    # lepiej więc pozbyć się tego printa:
+    # print(animal_sounds([dog, cat, cow]))
+    animal_sounds([dog, cat, cow])
 
 
-
-
-
-
+# Dobrą praktyką jest używanie poniższego w skryptach ;)
+if __name__ == '__main__':
+    main()
